1acb05d68b1b7bbebd0c6234643c17d968f6fe92
Boumarouane <i.boumarouane@gmail.com>
2021-10-25 02:09:33 +0200

    placement des grilles en css

---
+++ headphones-app/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="main-container">
+      <section className="blur-container">
+        <div className="grid-navbar">navbar</div>
+        <div className="grid-text-left">text-left</div>
+        <div className="grid-pagination">pagination</div>
+        <div className="grid-title">title</div>
+        <div className="grid-description">description-left</div>
+        {/* <div className="grid-description-right">description-right</div> */}
+        <div className="grid-slider-stars">slider-button</div>
+        {/* <div className="grid-stars">stars</div> */}
+        <div className="grid-canvas">canvas</div>
+        <div className="grid-social-icons">social-icons</div>
+        <div className="grid-text-right">text-right </div>
+      </section>
+    </section>
+  );
+}
+
+export default App;
