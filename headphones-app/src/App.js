f54ca35dfa9b127fbba0f6beac252b1c5bcb297e
Boumarouane <i.boumarouane@gmail.com>
2021-10-25 20:54:01 +0200

    ajout de la responsivité des grilles

--- headphones-app/src/App.js
+++ headphones-app/src/App.js
@@ -8,10 +8,14 @@ function App() {
         <div className="grid-text-left">text-left</div>
         <div className="grid-pagination">pagination</div>
         <div className="grid-title">title</div>
-        <div className="grid-description">description-left</div>
-        {/* <div className="grid-description-right">description-right</div> */}
-        <div className="grid-slider-stars">slider-button</div>
-        {/* <div className="grid-stars">stars</div> */}
+        <div className="grid-description">
+          <div>description-left</div>
+          <div>description-right</div>
+        </div>
+        <div className="grid-slider-stars">
+          <div>Slider button</div>
+          <div>Price and stars</div>
+        </div>
         <div className="grid-canvas">canvas</div>
         <div className="grid-social-icons">social-icons</div>
         <div className="grid-text-right">text-right </div>
